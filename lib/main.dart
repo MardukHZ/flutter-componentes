0e388aad6a0dffbea7536d8b0b5ad00442e261be
cvargas22 <cesar-ed22@hotmail.com>
2019-09-25 16:00:35 -0300

    testing push

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'App Componentes',
       //home: HomePage(),
       initialRoute: '/',
       routes: <String,WidgetBuilder>{
