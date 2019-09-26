f255fda9e90f0937f3cd1f625440354ff6d536a1
cvargas22 <cesar-ed22@hotmail.com>
2019-09-26 20:15:34 -0300

    alertpage

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -13,6 +13,49 @@ class AlertPage extends StatelessWidget {
         },
         child: Icon(Icons.keyboard_backspace),
       ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () => _mostrarAlert(context),
+          color: Colors.blueAccent,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          child: Text('Mostrar Alerta'),
+        ),
+      ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Alerta'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Esto es una alerta'),
+              FlutterLogo(size: 80.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+          //content: ,
+
+        );
+      }
+
+
     );
   }
 }
